68db0247c792eb789dac715e6f3d5276f66b854f
roxas332519096 <roxas332519096@vip.qq.com>
2019-03-01 20:49:07 +0800

    add draw page

--- src/views/Draw.vue
+++ src/views/Draw.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-      <el-row type="flex" justify="center">
-          <el-col  :span="3">
-            已抽 {{box.length}} 次
-          </el-col>
-          <el-col :span="3">
-              已出SSR{{hasSSR}}
-          </el-col>
-          <el-col :span="3">已出SR{{hasSR}}</el-col>
-      </el-row>
     <el-row type="flex" justify="center">
-      <el-col  :span="6">
+      <el-col :span="3">已抽 {{box.length}} 次</el-col>
+      <el-col :span="3">已出SSR{{hasSSR}}</el-col>
+      <el-col :span="3">已出SR{{hasSR}}</el-col>
+    </el-row>
+    <el-row type="flex" justify="center">
+      <el-col :span="6">
         <el-button @click="drawOnce">召唤1次</el-button>
         <el-button @click="drawTen">召唤10次</el-button>
         <el-button @click="box=[]">清空</el-button>
@@ -18,12 +14,11 @@
     </el-row>
     <el-row type="flex" justify="center">
       <el-col :span="10">
-          <el-row class="box">
-              <el-col :span="2"  v-for="(card,index) in box" :key="index">
-                <div class="card" :class="card">{{card}}</div>
-              </el-col>
-          </el-row>
-        </el-col>
+        <el-row class="box">
+          <el-col :span="2" v-for="(card,index) in box" :key="index">
+            <div class="card" :class="card">{{card}}</div>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
@@ -40,7 +35,6 @@ export default {
   methods: {
     drawOnce() {
       let once = Math.random() * 100;
-      console.log(once);
       if (once <= 1) {
         this.box.push("ssr");
       } else if (once <= 5) {
@@ -55,30 +49,45 @@ export default {
         this.box.push("三星礼装");
       }
     },
-      drawTen(){
-        for(let i = 0 ;i < 10;i++){
-            this.drawOnce()
-        }
-    }   
-  },
-  computed:{
-      hasSSR(){
-          return this.box.filter((card)=>{
-              return card === "ssr"
-          }).length
-      },
-      hasSR(){
-          return this.box.filter((card)=>{
-              return card === "sr"
-          }).length
+    drawTen() {
+      for (let i = 0; i < 9; i++) {
+        this.drawOnce();
       }
+      this.underDraw();
+    },
+    underDraw() {
+        let draw = Math.random() * 20;
+        console.log(draw)
+        if (draw <= 1) {
+        this.box.push("ssr");
+      } else if (draw <= 5) {
+        this.box.push("五星礼装");
+      } else if (draw <= 8) {
+        this.box.push("sr");
+      }else{
+          this.box.push('四星礼装')
+      }
+    }
+  },
+  computed: {
+    hasSSR() {
+      return this.box.filter(card => {
+        return card === "ssr";
+      }).length;
+    },
+    hasSR() {
+      return this.box.filter(card => {
+        return card === "sr";
+      }).length;
+    }
   }
 };
 </script>
 
 <style lang="scss" scope>
-span,div{
-    box-sizing: border-box;
+span,
+div {
+  box-sizing: border-box;
 }
 
 .box {
@@ -86,7 +95,7 @@ span,div{
   min-height: 200px;
 }
 .card {
-    margin:10px;
+  margin: 10px;
   width: 50px;
   height: 100px;
   background: gray;
