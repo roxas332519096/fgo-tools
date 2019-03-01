8856b564e6837e75278ff4e7b7858e91cf59d512
roxas332519096 <roxas332519096@vip.qq.com>
2019-03-01 22:38:17 +0800

    2233

--- src/views/Draw.vue
+++ src/views/Draw.vue
@@ -1,26 +1,47 @@
 <template>
   <div>
     <el-row type="flex" justify="center">
-      <el-col :span="3">已抽 {{box.length}} 次</el-col>
-      <el-col :span="3">已出SSR{{hasSSR}}</el-col>
-      <el-col :span="3">已出SR{{hasSR}}</el-col>
-    </el-row>
-    <el-row type="flex" justify="center">
-      <el-col :span="6">
-        <el-button @click="drawOnce">召唤1次</el-button>
-        <el-button @click="drawTen">召唤10次</el-button>
-        <el-button @click="box=[]">清空</el-button>
+      <el-col :xs="12" :xl="3">
+        <label>已有呼符</label>
+        <el-input v-model="charm" placeholder="已有呼符"></el-input>
+      </el-col>
+      <el-col :xs="12" :xl="3">
+        <label>已有石头</label>
+        <el-input v-model="stone" placeholder="已有圣晶石"></el-input>
       </el-col>
     </el-row>
     <el-row type="flex" justify="center">
-      <el-col :span="10">
-        <el-row class="box">
-          <el-col :span="2" v-for="(card,index) in box" :key="index">
-            <div class="card" :class="card">{{card}}</div>
-          </el-col>
-        </el-row>
+      <el-col :xs="8" :xl="3">
+        <span>剩余石头{{stone}}</span>
+      </el-col>
+      <el-col :xs="8" :xl="3">
+        <span>已充{{money}}元</span>
+      </el-col>
+      <el-col :xs="8" :xl="3">
+        <el-button @click="pay">充一单</el-button>
       </el-col>
     </el-row>
+    <el-row type="flex" justify="center">
+      <el-col :xs="12" :xl="3">已抽 {{box.length}} 次</el-col>
+      <el-col :xs="12" :xl="3">已出SSR{{hasSSR}}</el-col>
+      <el-col :xs="12" :xl="3">当期SSR{{hasSSRUP}}</el-col>
+      <el-col :xs="12" :xl="3">歪SSR{{hasSSRNOUP}}</el-col>
+      <el-col :xs="12" :xl="3">已出SR{{hasSR}}</el-col>
+    </el-row>
+    <el-row type="flex" justify="center">
+      <el-col :xs="8" :xl="3">
+        <el-button @click="charmOrStone" :disabled="stone < 3 && charm < 1">召唤1次</el-button>
+      </el-col>
+      <el-col :xs="8" :xl="3">
+        <el-button @click="drawTen" :disabled="stone < 30">召唤10次</el-button>
+      </el-col>
+      <el-col :xs="8" :xl="3">
+        <el-button @click="clear">清空</el-button>
+      </el-col>
+    </el-row>
+    <div class="box">
+      <div v-for="(card,index) in box" class="card" :class="card" :key="index">{{card}}</div>
+    </div>
   </div>
 </template>
 
@@ -29,14 +50,36 @@ export default {
   name: "Draw",
   data() {
     return {
-      box: []
+      box: [],
+      charm: 0,
+      stone: 0,
+      money: 0
     };
   },
   methods: {
+    pay() {
+      this.stone += 167;
+      this.money += 518;
+    },
+    clear() {
+      this.stone = 0;
+      this.money = 0;
+      this.charm = 0;
+      this.box = [];
+    },
+    charmOrStone() {
+      if (this.charm >= 1) {
+        this.charm--;
+        this.drawOnce();
+      } else if (this.stone >= 3) {
+        this.stone -= 3;
+        this.drawOnce();
+      }
+    },
     drawOnce() {
       let once = Math.random() * 100;
       if (once <= 1) {
-        this.box.push("ssr");
+        this.drawSSR();
       } else if (once <= 5) {
         this.box.push("五星礼装");
       } else if (once <= 8) {
@@ -50,29 +93,46 @@ export default {
       }
     },
     drawTen() {
+      if (this.stone < 30) return;
+      this.stone -= 30;
       for (let i = 0; i < 9; i++) {
         this.drawOnce();
       }
       this.underDraw();
     },
     underDraw() {
-        let draw = Math.random() * 20;
-        console.log(draw)
-        if (draw <= 1) {
-        this.box.push("ssr");
+      let draw = Math.random() * 20;
+      if (draw <= 1) {
+        this.drawSSR();
       } else if (draw <= 5) {
         this.box.push("五星礼装");
       } else if (draw <= 8) {
         this.box.push("sr");
-      }else{
-          this.box.push('四星礼装')
+      } else {
+        this.box.push("四星礼装");
+      }
+    },
+    drawSSR() {
+      let rang = Math.random() * 100;
+      if (rang < 60) {
+        this.box.push("ssr up");
+      } else {
+        this.box.push("ssr noup");
       }
     }
   },
   computed: {
     hasSSR() {
+      return this.hasSSRUP + this.hasSSRNOUP;
+    },
+    hasSSRUP() {
       return this.box.filter(card => {
-        return card === "ssr";
+        return card === "ssr up";
+      }).length;
+    },
+    hasSSRNOUP() {
+      return this.box.filter(card => {
+        return card === "ssr noup";
       }).length;
     },
     hasSR() {
@@ -92,11 +152,15 @@ div {
 
 .box {
   margin-top: 20px;
-  min-height: 200px;
+  display: flex;
+  width: 80vw;
+  max-height: 50vh;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 .card {
   margin: 10px;
-  width: 50px;
+  width: 80px;
   height: 100px;
   background: gray;
   &.ssr {
@@ -104,6 +168,9 @@ div {
   }
   &.sr {
     background: green;
+  }
+  &.up{
+      background: red;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    {{ msg }} Ver:{{ ver }}
+    <div>{{ msg }} Ver:{{ ver }} 作者:{{author}}</div>
+    <div v-for="ver in update" :key="ver.ver">
+      <div class="ver">
+        日期:{{ver.data}},版本:{{ver.ver}},更新日志{{ver.msg}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'home',
-  data(){
-    return{
-      msg:'非洲Fgo计算工具',
-      ver:'0.0.1'
-    }
+  name: "home",
+  data() {
+    return {
+      msg: "非洲Fgo计算工具",
+      ver: "0.0.1",
+      author:'不吃虫子的鸡',
+      update: [
+        {
+          data: "2019-3-1",
+          ver: "0.0.1",
+          msg: "添加英灵材料计算"
+        },
+        {
+          data: "2019-3-1",
+          ver: "0.0.2",
+          msg: "添加模拟抽卡"
+        }
+      ]
+    };
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+  .ver{
+    margin: 20px;
+  }
+</style>
+
